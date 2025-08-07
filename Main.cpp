26c4b1e0eace524cd831fe121fbad0dc78835304
studentAyBu <23050161004@aybu.edu.tr>
2025-08-07 14:14:09 +0300

    FirstCommit

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "arSDL/arSDL.h" // Include my SDL2 OOP wrapper
+
+#define COINS 30 // Number of coins
+
+#define IDLE 0 // Mario can be idle, walk or jump
+#define WALK 1
+#define JUMP 2
+
+
+int main() {
+   
+    return 0;
+}
